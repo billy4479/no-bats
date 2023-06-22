dbe3fa119cf77c41abbadb7d7151449100fd9967
billy4479 <giachi.ellero@gmail.com>
2023-06-22 18:36:29 +0200

    Use Fabric-API to edit the biome spawn settings
    instead of that shitty mixing of before

--- src/main/java/online/polpetta/nobats/NoBats.java
+++ src/main/java/online/polpetta/nobats/NoBats.java
@@ -1,20 +1,34 @@
 package online.polpetta.nobats;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.fabricmc.fabric.api.biome.v1.ModificationPhase;
+import net.minecraft.entity.EntityType;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class NoBats implements ModInitializer {
+
+  public static final String MOD_ID = "no-bats";
+
   // This logger is used to write text to the console and the log file.
   // It is considered best practice to use your mod id as the logger's name.
   // That way, it's clear which mod wrote info, warnings, and errors.
-  public static final Logger LOGGER = LoggerFactory.getLogger("no-bats");
+  public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
   @Override
   public void onInitialize() {
     // This code runs as soon as Minecraft is in a mod-load-ready state.
     // However, some things (like resources) may still be uninitialized.
     // Proceed with mild caution.
+
+    BiomeModifications.create(new Identifier(MOD_ID))
+        .add(ModificationPhase.REMOVALS, BiomeSelectors.all(),
+             (context
+              -> context.getSpawnSettings().removeSpawnsOfEntityType(
+                  EntityType.BAT)));
 
     LOGGER.info("[NoBats] Bats will no longer spawn");
   }
